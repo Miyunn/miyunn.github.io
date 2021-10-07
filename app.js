3c80fbb48dd72603ed2a584c0fff9b4b27bde181
Miyunn <kavintheperera@gmail.com>
2021-10-07 21:42:39 +0530

    Working App

---
+++ app.js
@@ -0,0 +1,16 @@
+const figets = ["Pop Open", "Close Shut", "Spin", "Spin Faster", 
+"Triple Spin", "Double Spin", "Un Oh You almost dropped it",
+ "Woohoo Quadruple Spin"];
+const btn = document.getElementById("btn");
+const fidget = document.querySelector(".fidget");
+
+btn.addEventListener("click", function(){
+    const randomFiget = getRandomFiget();
+    console.log(randomFiget);
+    fidget.textContent = figets[randomFiget];
+});
+
+function getRandomFiget(){
+    return Math.floor(Math.random() * figets.length);
+}
+
